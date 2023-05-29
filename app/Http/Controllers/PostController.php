a5e5edba895b38a1fc6a0d065123eee5bba28fd8
klelia <clelia.fradella@gmail.com>
2023-05-29 12:25:37 +0200

    crud per post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,50 +10,58 @@ class PostController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     *
      */
     public function index()
     {
-        //
+
+        $data = [
+            'posts' => Post::all()
+        ];
+        return view('posts.index', $data);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     *
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     *
      */
     public function store(Request $request)
     {
-        //
+        $form_data = $request->all();
+        $newPost = new Post();
+        $newPost->fill($form_data);
+        $newPost->save();
+        return redirect()->route('posts.show', $newPost->id);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     *
      */
     public function show(Post $post)
     {
-        //
+        return view('posts.show', compact('post'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     *
      */
     public function edit(Post $post)
     {
@@ -65,7 +73,7 @@ class PostController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     *
      */
     public function update(Request $request, Post $post)
     {
